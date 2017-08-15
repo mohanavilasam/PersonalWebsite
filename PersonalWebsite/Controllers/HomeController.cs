560f21eb5f8ff7b6609b053a7ae99eca13c62d02
Maneesh Mohanavilasm <maneesh.m@aggiemail.usu.edu>
2017-08-15 07:46:39 -0600

    Added email client to the contact form

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalWebsite.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace PersonalWebsite.Controllers
 {
@@ -32,6 +36,36 @@ namespace PersonalWebsite.Controllers
             ViewBag.Message = "Page under construction.";
 
             return View();
+        }
+
+        public ActionResult Sent()
+        {
+            ViewBag.Message = "Your message has been successfully sent. Thank you for contacting me.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("maneesh.m@aggiemail.usu.edu")); 
+                message.From = new MailAddress("tdg-inq-msg@outlook.com"); 
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.FromMessage);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ PersonalWebsite/Models/ContactFormModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalWebsite.Models
+{
+    public class ContactFormModel
+    {
+        [Required, StringLength(70), Display(Name = "Your name")]
+        public string FromName { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Your Email")]
+        public string FromEmail { get; set; }
+
+        [Required, StringLength(15), Display(Name = "Phone Number")]
+        public string FromPhone { get; set; }
+
+        [Required, StringLength(510), Display(Name = "Message")]
+        public string FromMessage { get; set; } 
+    }
+}
